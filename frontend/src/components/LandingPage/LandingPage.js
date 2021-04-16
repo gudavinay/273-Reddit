231444361e240e6ea8ae744e696836bd576914a3
vinay guda <vinayguda05@gmail.com>
2021-04-15 21:25:16 -0700

    reddit basic icons svg

--- frontend/src/components/LandingPage/LandingPage.js
+++ frontend/src/components/LandingPage/LandingPage.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
-import { REDDIT } from '../../services/Constants'
+import { REDDIT } from '../../services/Constants';
+import redditLogoSVG from '../../assets/redditLogo.svg';
+import redditTextSVG from '../../assets/redditText.svg';
 
 class LandingPage extends Component {
 
     render() {
         return (
             <React.Fragment>
-                {REDDIT}
+                <div style={{margin:'auto'}}>
+                    <img style={{ height: '50px', width: '50px' }} alt="Reddit Logo" src={redditLogoSVG} />
+                    <img style={{ height: '50px', width: '50px' }} alt="Reddit Logo" src={redditTextSVG} />
+                    {REDDIT}
+                </div>
             </React.Fragment>
         )
     }
