85167af9bba6f5600aa6d2711766d69ffa6b3657
vinay guda <vinayguda05@gmail.com>
2021-04-17 19:28:47 -0700

    mongodb connection

---
+++ backend/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI:"mongodb+srv://reddit-user:reddit@1234@reddit-cluster.vvqtv.mongodb.net/reddit-db",
+    secret: "reddit_secret_key",
+    frontEnd: "http://localhost:3000"
+}
