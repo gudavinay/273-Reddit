87b587941f478a94ef14af1b757d180992d98575
divya mittal <divyamittal@divyas-MacBook-Pro.local>
2021-04-17 19:56:38 -0700

    Added es lint ,jwt and kafka initial client files

--- frontend/src/reduxOps/reduxReducers/signupReducer.js
+++ frontend/src/reduxOps/reduxReducers/signupReducer.js
@@ -1,14 +1,14 @@
 import { SIGNUP } from "../types";
 
 const initialState = {
-    user: {}
+  user: {}
 };
 
 export default function redux(state = initialState, action) {
-    switch (action.type) {
-        case SIGNUP:
-            return { ...state, user: action.payload };
-        default:
-            return state;
-    }
-};
+  switch (action.type) {
+    case SIGNUP:
+      return { ...state, user: action.payload };
+    default:
+      return state;
+  }
+}
